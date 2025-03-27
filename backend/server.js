0121968eaa3c75dfb786b972b69c3905a9cab029
Amer <amerlopo5@gmail.com>
2025-03-27 20:30:49 +0100

    .

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
@@ -7,21 +7,25 @@ app.use(express.json());
 const words = ["APPLE", "TIGER", "HOUSE", "CHECK", "PIZZA", "AMAR"];
 
 const word = [];
+const guess = [];
 
 app.get("/word", async (req, res) => {
-    const randomWord = words[Math.floor(Math.random() * words.length)];
-    const hiddenWord = [];
-    for (let i = 0; i < randomWord.length; i++) {
-        hiddenWord.push('_');
-        word.push(randomWord[i]);
-    }
-    return res.json({length: randomWord.length, hiddenWord, randomWord, word});
-})
+  const randomWord = words[Math.floor(Math.random() * words.length)];
+  for (let i = 0; i < randomWord.length; i++) {
+    guess.push("_");
+    word.push(randomWord[i]);
+  }
+  return res.json({ length: randomWord.length, guess, randomWord, word });
+});
 
-app.post('/check', async (req, res) => {
-    const letter = req.body;
-})
+app.post("/check", async (req, res) => {
+  const letter = req.body;
+
+  if (word.includes(letter)) {
+    const indexes = letter
+  }
+});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
